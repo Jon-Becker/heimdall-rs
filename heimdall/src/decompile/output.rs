30f3a655ead06688047b64faf9f782d6e083cd26
idc <idc@nope>
2022-10-03 16:26:15 +0200

    fix(decompile) restore write output to file

--- heimdall/src/decompile/output.rs
+++ heimdall/src/decompile/output.rs
@@ -1,6 +1,6 @@
 use std::time::Duration;
 
-use heimdall_common::io::{logging::{TraceFactory, Logger}, file::{short_path, write_file}};
+use heimdall_common::io::{logging::{TraceFactory, Logger}, file::{short_path, write_file, write_lines_to_file}};
 use indicatif::ProgressBar;
 
 use super::{DecompilerArgs, util::Function, constants::DECOMPILED_SOURCE_HEADER, postprocess::postprocess};
@@ -397,10 +397,10 @@ pub fn build_output(
         
     }
 
-    // write_lines_to_file(
-    //     &decompiled_output_path,
-    //     decompiled_output
-    // );
-    //
-    // logger.info(&format!("wrote decompiled solidity to '{}' .", decompiled_output_path).to_string());
+    write_lines_to_file(
+        &decompiled_output_path,
+        decompiled_output
+    );
+    
+    logger.info(&format!("wrote decompiled solidity to '{}' .", decompiled_output_path).to_string());
 }
