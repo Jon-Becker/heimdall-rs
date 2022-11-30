502cf07cf5b935dc726211e46fb8f03c5c3ecbc2
Danyal Prout <danyal@paradigm.xyz>
2022-11-30 11:53:46 -0800

    fix(decompile): use 32 byte hash for events to reduce collisions

--- heimdall/src/decompile/mod.rs
+++ heimdall/src/decompile/mod.rs
@@ -357,7 +357,7 @@ pub fn decompile(args: DecompilerArgs) {
         }
 
         if !args.skip_resolving {
-            
+
             let resolved_functions = match resolved_selectors.get(&selector) {
                 Some(func) => func.clone(),
                 None => {
@@ -480,8 +480,8 @@ pub fn decompile(args: DecompilerArgs) {
             // resolve custom event signatures
             resolved_counter = 0;
             for (event_selector, (_, raw_event)) in analyzed_function.events.clone() {
-                decompilation_progress.set_message(format!("resolving event 0x{}", &event_selector.get(0..8).unwrap().to_string()));
-                let resolved_event_selectors = resolve_event_signature(&event_selector.get(0..8).unwrap().to_string());
+                decompilation_progress.set_message(format!("resolving event 0x{}", &event_selector.get(0..64).unwrap().to_string()));
+                let resolved_event_selectors = resolve_event_signature(&event_selector.get(0..64).unwrap().to_string());
 
                 // only continue if we have matches
                 match resolved_event_selectors {
@@ -507,7 +507,7 @@ pub fn decompile(args: DecompilerArgs) {
                                 std::process::exit(1)
                             }
                         };
-                        
+
                         resolved_counter += 1;
                         analyzed_function.events.insert(event_selector, (Some(selected_match.clone()), raw_event));
                     },
