afe4f9dd4c711d065b83796d8ad088236d8cbf2d
Jonathan Becker <64037729+Jon-Becker@users.noreply.github.com>
2023-03-14 17:56:36 -0500

    :wrench: fix: remove `clipboard`

--- common/src/io/clipboard.rs
+++ common/src/io/clipboard.rs
@@ -1,7 +1,4 @@
-extern crate clipboard;
-use clipboard::{ClipboardProvider, ClipboardContext};
 
 pub fn copy_to_clipboard(text: &str) {
-    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-    ctx.set_contents(text.to_string()).unwrap();
+    
 }
