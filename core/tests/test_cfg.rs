7e712c90af916adedf1633131a5d194fe1960049
Jon-Becker <jonathan@jbecker.dev>
2023-09-28 13:39:56 -0400

    ✅ tests: refactor `cfg` tests

--- core/tests/test_cfg.rs
+++ core/tests/test_cfg.rs
@@ -1,89 +1,91 @@
 #[cfg(test)]
 mod benchmark {
     use clap_verbosity_flag::Verbosity;
-    use heimdall_common::testing::benchmarks::benchmark;
+    use heimdall_common::testing::benchmarks::async_bench;
 
     use heimdall_core::cfg::CFGArgs;
 
-    #[test]
-    fn benchmark_generate_cfg_simple() {
-        fn bench() {
+    #[tokio::test]
+    async fn benchmark_generate_cfg_simple() {
+        async fn bench() {
             let args = CFGArgs {
                 target: String::from("0x1bf797219482a29013d804ad96d1c6f84fba4c45"),
                 verbose: Verbosity::new(0, 0),
-                output: String::from(""),
                 rpc_url: String::from("https://eth.llamarpc.com"),
                 default: true,
                 color_edges: false,
-                format: String::from(""),
+                format: String::from("png"),
             };
-            heimdall_core::cfg::cfg(args)
+            let _ = heimdall_core::cfg::cfg(args).await;
         }
 
-        benchmark("benchmark_generate_cfg_simple", 100, bench)
+        async_bench("benchmark_generate_cfg_simple", 100, bench).await;
     }
 
-    #[test]
-    fn benchmark_generate_cfg_complex() {
-        fn bench() {
+    #[tokio::test]
+    async fn benchmark_generate_cfg_complex() {
+        async fn bench() {
             let args = CFGArgs {
                 target: String::from("0xE90d8Fb7B79C8930B5C8891e61c298b412a6e81a"),
                 verbose: Verbosity::new(0, 0),
-                output: String::from(""),
                 rpc_url: String::from("https://eth.llamarpc.com"),
                 default: true,
                 color_edges: false,
-                format: String::from(""),
+                format: String::from("png"),
             };
-            heimdall_core::cfg::cfg(args)
+            let _ = heimdall_core::cfg::cfg(args).await;
         }
 
-        benchmark("benchmark_generate_cfg_complex", 100, bench)
+        async_bench("benchmark_generate_cfg_complex", 100, bench).await;
     }
 }
 
 #[cfg(test)]
 mod integration_tests {
-    use heimdall_common::io::file::{delete_path, read_file};
+    use clap_verbosity_flag::Verbosity;
+    use heimdall_core::cfg::CFGArgs;
+    use petgraph::dot::Dot;
 
-    use heimdall_core::cfg::CFGBuilder;
+    #[tokio::test]
+    async fn test_cfg_simple() {
+        let result = heimdall_core::cfg::cfg(CFGArgs {
+            target: String::from("0x1bf797219482a29013d804ad96d1c6f84fba4c45"),
+            verbose: Verbosity::new(0, 0),
+            rpc_url: String::from("https://eth.llamarpc.com"),
+            default: true,
+            color_edges: false,
+            format: String::from("png"),
+        })
+        .await
+        .unwrap();
 
-    #[test]
-    fn test_cfg_simple() {
-        let expected_lines = vec![
+        let output = format!("{}", Dot::with_config(&result, &[]));
+
+        for line in &[
             String::from("0 [ label = \"0x01 PUSH20 0x1bf797219482a29013d804ad96d1c6f84fba4c45\\l0x16 ADDRESS \\l0x17 EQ \\l0x18 PUSH1 0x80\\l0x1a PUSH1 0x40\\l0x1c MSTORE \\l0x1d PUSH1 0x04\\l0x1f CALLDATASIZE \\l0x20 LT \\l0x21 PUSH2 0x58\\l0x24 JUMPI \\l\" ]"),
             String::from("0 -> 13 []")
-        ];
-
-        CFGBuilder::new("0x1bf797219482a29013d804ad96d1c6f84fba4c45")
-            .rpc("https://eth.llamarpc.com")
-            .output("./output/tests/cfg/test1")
-            .generate();
-
-        let dot = read_file(&String::from("./output/tests/cfg/test1/cfg.dot"));
-
-        for line in expected_lines {
-            assert!(dot.contains(&line))
+        ] {
+            output.contains(line);
         }
-
-        delete_path(&String::from("./output/tests/cfg/test1"));
     }
 
-    #[test]
-    fn test_cfg_complex() {
-        let expected_lines = vec![String::from("\"0x03a0 JUMPDEST \\l0x03a1 STOP \\l\"")];
+    #[tokio::test]
+    async fn test_cfg_complex() {
+        let result = heimdall_core::cfg::cfg(CFGArgs {
+            target: String::from("0xE90d8Fb7B79C8930B5C8891e61c298b412a6e81a"),
+            verbose: Verbosity::new(0, 0),
+            rpc_url: String::from("https://eth.llamarpc.com"),
+            default: true,
+            color_edges: false,
+            format: String::from("png"),
+        })
+        .await
+        .unwrap();
 
-        CFGBuilder::new("0xE90d8Fb7B79C8930B5C8891e61c298b412a6e81a")
-            .rpc("https://eth.llamarpc.com")
-            .output("./output/tests/cfg/test2/")
-            .generate();
+        let output = format!("{}", Dot::with_config(&result, &[]));
 
-        let dot = read_file(&String::from("./output/tests/cfg/test2/cfg.dot"));
-
-        for line in expected_lines {
-            assert!(dot.contains(&line))
+        for line in &[String::from("\"0x03a0 JUMPDEST \\l0x03a1 STOP \\l\"")] {
+            assert!(output.contains(&*line))
         }
-
-        delete_path(&String::from("./output/tests/cfg/test2"));
     }
 }
