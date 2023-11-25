8ca1b0a8b03ad831d0450aedd19f6988d7259b31
Ian Guimaraes <ianguimaraes31@gmail.com>
2023-11-25 13:04:38 -0300

    chore: merge with origin branch

--- core/src/snapshot/mod.rs
+++ core/src/snapshot/mod.rs
@@ -119,37 +119,8 @@ pub async fn snapshot(args: SnapshotArgs) -> Result<SnapshotResult, Box<dyn std:
         "()".to_string(),
     );
 
-    let contract_bytecode: String;
-    if ADDRESS_REGEX.is_match(&args.target)? {
-        // We are snapshotting a contract address, so we need to fetch the bytecode from the RPC
-        // provider.
-        contract_bytecode = get_code(&args.target, &args.rpc_url).await?;
-    } else if BYTECODE_REGEX.is_match(&args.target)? {
-        logger.debug_max("using provided bytecode for snapshotting.");
-        contract_bytecode = args.target.clone().replacen("0x", "", 1);
-    } else {
-        logger.debug_max("using provided file for snapshotting.");
+    let contract_bytecode = get_contract_bytecode(&args.target, &args.rpc_url, &logger).await?;
 
-        // We are snapshotting a file, so we need to read the bytecode from the file.
-        contract_bytecode = match fs::read_to_string(&args.target) {
-            Ok(contents) => {
-                let _contents = contents.replace('\n', "");
-                if BYTECODE_REGEX.is_match(&_contents)? && _contents.len() % 2 == 0 {
-                    _contents.replacen("0x", "", 1)
-                } else {
-                    logger
-                        .error(&format!("file '{}' doesn't contain valid bytecode.", &args.target));
-                    std::process::exit(1)
-                }
-            }
-            Err(_) => {
-                logger.error(&format!("failed to open file '{}' .", &args.target));
-                std::process::exit(1)
-            }
-        };
-    }
-
-    // disassemble the bytecode
     let disassembled_bytecode = disassemble(DisassemblerArgs {
         target: contract_bytecode.clone(),
         verbose: args.verbose.clone(),
@@ -158,6 +129,7 @@ pub async fn snapshot(args: SnapshotArgs) -> Result<SnapshotResult, Box<dyn std:
         output: String::new(),
     })
     .await?;
+
     trace.add_call(
         snapshot_call,
         line!(),
@@ -195,12 +167,7 @@ pub async fn snapshot(args: SnapshotArgs) -> Result<SnapshotResult, Box<dyn std:
         0,
         u128::max_value(),
     );
-    let mut shortened_target = contract_bytecode.clone();
-    if shortened_target.len() > 66 {
-        shortened_target = shortened_target.chars().take(66).collect::<String>() +
-            "..." +
-            &shortened_target.chars().skip(shortened_target.len() - 16).collect::<String>();
-    }
+    let shortened_target = get_shortned_target(&contract_bytecode);
     let vm_trace = trace.add_creation(
         snapshot_call,
         line!(),
@@ -578,9 +545,44 @@ fn get_shortned_target(target: &String) -> String {
     shortened_target
 }
 
+async fn get_contract_bytecode(
+    target: &str,
+    rpc_url: &str,
+    logger: &Logger,
+) -> Result<String, Box<dyn std::error::Error>> {
+    if ADDRESS_REGEX.is_match(target)? {
+        // We are snapshotting a contract address, so we need to fetch the bytecode from the RPC
+        // provider.
+        get_code(target, rpc_url).await
+    } else if BYTECODE_REGEX.is_match(target)? {
+        logger.debug_max("using provided bytecode for snapshotting.");
+        Ok(target.replacen("0x", "", 1))
+    } else {
+        logger.debug_max("using provided file for snapshotting.");
+
+        // We are snapshotting a file, so we need to read the bytecode from the file.
+        match fs::read_to_string(target) {
+            Ok(contents) => {
+                let _contents = contents.replace('\n', "");
+                if BYTECODE_REGEX.is_match(&_contents)? && _contents.len() % 2 == 0 {
+                    Ok(_contents.replacen("0x", "", 1))
+                } else {
+                    logger.error(&format!("file '{}' doesn't contain valid bytecode.", &target));
+                    std::process::exit(1)
+                }
+            }
+            Err(_) => {
+                logger.error(&format!("failed to open file '{}' .", &target));
+                std::process::exit(1)
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use fancy_regex::Regex;
     use std::env;
 
     #[test]
@@ -608,5 +610,55 @@ mod tests {
         let shortened_target = get_shortned_target(&short_target);
 
         assert_eq!(shortened_target.len(), 66);
+    }
+
+    #[tokio::test]
+    async fn test_get_bytecode_when_target_is_address() {
+        let bytecode_regex = Regex::new(r"^[0-9a-fA-F]{0,50000}$").unwrap();
+        let (logger, _) = get_logger_and_trace(&clap_verbosity_flag::Verbosity::new(-1, 0));
+        let bytecode = get_contract_bytecode(
+            "0x9f00c43700bc0000Ff91bE00841F8e04c0495000",
+            "https://eth.llamarpc.com",
+            &logger,
+        )
+        .await
+        .unwrap();
+
+        assert!(bytecode_regex.is_match(&bytecode).unwrap());
+        // Not possible to express with regex since fancy_regex
+        // doesn't support look-arounds
+        assert!(!bytecode.starts_with("0x"));
+    }
+
+    #[tokio::test]
+    async fn test_get_bytecode_when_target_is_bytecode() {
+        let bytecode_regex = Regex::new(r"^[0-9a-fA-F]{0,50000}$").unwrap();
+        let (logger, _) = get_logger_and_trace(&clap_verbosity_flag::Verbosity::new(-1, 0));
+        let bytecode = get_contract_bytecode(
+            "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001",
+            "https://eth.llamarpc.com",
+            &logger,
+        ).await.unwrap();
+
+        assert!(bytecode_regex.is_match(&bytecode).unwrap());
+        assert!(!bytecode.starts_with("0x"));
+    }
+
+    #[tokio::test]
+    async fn test_get_bytecode_when_target_is_file_path() {
+        let (logger, _) = get_logger_and_trace(&clap_verbosity_flag::Verbosity::new(-1, 0));
+        let bytecode_regex = Regex::new(r"^[0-9a-fA-F]{0,50000}$").unwrap();
+        let file_path = "./mock-file.txt";
+        let mock_bytecode = "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001";
+
+        fs::write(file_path, mock_bytecode).unwrap();
+
+        let bytecode =
+            get_contract_bytecode(file_path, "https://eth.llamarpc.com", &logger).await.unwrap();
+
+        assert!(bytecode_regex.is_match(&bytecode).unwrap());
+        assert!(!bytecode.starts_with("0x"));
+
+        fs::remove_file(file_path).unwrap();
     }
 }
