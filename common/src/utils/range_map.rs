a21c58df630a1a4e45659027f902ac3707665900
jeffchen006 <zhiychen@cs.toronto.edu>
2024-01-11 22:28:00 +0800

    fix a bug

--- common/src/utils/range_map.rs
+++ common/src/utils/range_map.rs
@@ -2,13 +2,6 @@ use std::{collections::HashMap, ops::Range};
 
 use crate::ether::evm::core::opcodes::WrappedOpcode;
 
-#[derive(Copy, Clone, Debug)]
-enum CollisionKind {
-    Deletion,
-    Splitting,
-    Shortening,
-}
-
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub struct RangeMap(pub HashMap<Range<usize>, WrappedOpcode>);
 
@@ -39,64 +32,48 @@ impl RangeMap {
             self.0.insert(range, opcode);
         } else {
             incumbents.iter().for_each(|incumbent| {
-                match Self::classify_collision(&range, incumbent) {
-                    CollisionKind::Deletion => {
+                if incumbent.end < range.start {}
+                else if incumbent.start > range.end {}
+                else {
+                    // Case 1: overlapping
+                    if range.start <= incumbent.start && range.end >= incumbent.end {
+                        // newInterval completely covers incumbent
+                        // remove the existing interval
                         self.0.remove(incumbent);
                     }
-                    CollisionKind::Splitting => {
-                        let left: Range<usize> =
-                            Range { start: incumbent.start, end: range.start - 1 };
-                        let right: Range<usize> =
-                            Range { start: range.end + 1, end: incumbent.end };
-                        let old_opcode: WrappedOpcode = self.0.get(incumbent).expect("").clone();
-
+                    // Case 2: shortening
+                    else if range.start <= incumbent.start && range.end < incumbent.end {
+                        let remainder: Range<usize> = Range { start: range.end + 1, end: incumbent.end };
+                        let old_opcode: WrappedOpcode = self.0.get(incumbent).cloned().unwrap();
+                        self.0.remove(incumbent);
+                        self.0.insert(remainder, old_opcode);
+                        
+                    } else if range.start > incumbent.start && range.end >= incumbent.end {
+                        let remainder: Range<usize> = Range { start: incumbent.start, end: range.start - 1 };
+                        let old_opcode: WrappedOpcode = self.0.get(incumbent).cloned().unwrap();
+                        self.0.remove(incumbent);
+                        self.0.insert(remainder, old_opcode);
+                        
+                    } 
+                    // Case 3: splitting
+                    else if range.start > incumbent.start && range.end < incumbent.end {    
+                        let left: Range<usize> = Range { start: incumbent.start, end: range.start - 1 };
+                        let right: Range<usize> = Range { start: range.end + 1, end: incumbent.end };
+                        let old_opcode: WrappedOpcode = self.0.get(incumbent).cloned().unwrap();
                         self.0.remove(incumbent);
                         self.0.insert(left, old_opcode.clone());
                         self.0.insert(right, old_opcode.clone());
-                    }
-                    CollisionKind::Shortening => {
-                        let needs_right_shortening =
-                            |incoming: &Range<usize>, incumbent: &Range<usize>| {
-                                incoming.start >= incumbent.start
-                            };
-
-                        if needs_right_shortening(&range, incumbent) {
-                            let remainder: Range<usize> =
-                                Range { start: incumbent.start, end: range.start - 1 };
-                            let old_opcode: WrappedOpcode = self.0.get(incumbent).cloned().unwrap();
-                            self.0.remove(incumbent);
-                            self.0.insert(remainder, old_opcode);
-                        } else {
-                            let remainder: Range<usize> =
-                                Range { start: range.end + 1, end: incumbent.end };
-                            let old_opcode: WrappedOpcode = self.0.get(incumbent).cloned().unwrap();
-                            self.0.remove(incumbent);
-                            self.0.insert(remainder, old_opcode);
-                        }
+                    } 
+                    else {
+                        // raise an error: this should be impossible
+                        assert!(false, "range_map::write: impossible case");
                     }
                 }
-
                 self.0.insert(range.clone(), opcode.clone());
             });
         }
     }
 
-    fn classify_collision(incoming: &Range<usize>, incumbent: &Range<usize>) -> CollisionKind {
-        let range_needs_deletion = |incoming: &Range<usize>, incumbent: &Range<usize>| {
-            incoming.start <= incumbent.start && incoming.end >= incumbent.end
-        };
-        let range_needs_splitting = |incoming: &Range<usize>, incumbent: &Range<usize>| {
-            incoming.start > incumbent.start && incoming.end < incumbent.end
-        };
-
-        if range_needs_deletion(incoming, incumbent) {
-            CollisionKind::Deletion
-        } else if range_needs_splitting(incoming, incumbent) {
-            CollisionKind::Splitting
-        } else {
-            CollisionKind::Shortening
-        }
-    }
 
     fn find_range(&self, offset: usize) -> Option<&Range<usize>> {
         self.0.keys().find(|range| range.contains(&offset))
