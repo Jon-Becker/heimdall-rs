a85c37591f12e7481386c85b7a94abd6ce5e029b
SFYLL <santiagoflood@hotmail.fr>
2024-01-26 09:55:12 +0100

    revert

--- common/src/utils/io/macros.rs
+++ common/src/utils/io/macros.rs
@@ -1,9 +1,9 @@
 #[macro_export]
 macro_rules! debug_max {
     ($message:expr) => {
-        $crate::utils::io::logging::Logger::default().debug($message);
+        $crate::utils::io::logging::Logger::default().debug_max($message);
     };
     ($message:expr, $($arg:tt)*) => {
-        $crate::utils::io::logging::Logger::default().debug(&format!($message, $($arg)*))
+        $crate::utils::io::logging::Logger::default().debug_max(&format!($message, $($arg)*))
     };
 }
