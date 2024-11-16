a9dd39d4fd157b6de99afd1e1826b4c0afca9aba
Jon-Becker <jonathan@jbecker.dev>
2024-11-16 16:47:04 -0600

    fix: `--abi` should default to `None`

--- crates/decode/src/interfaces/args.rs
+++ crates/decode/src/interfaces/args.rs
@@ -45,7 +45,7 @@ pub struct DecodeArgs {
     pub skip_resolving: bool,
 
     /// Path to an optional ABI file to use for resolving errors, functions, and events.
-    #[clap(long, short, default_value = "", hide_default_value = true)]
+    #[clap(long, short, default_value = None, hide_default_value = true)]
     pub abi: Option<String>,
 }
 
